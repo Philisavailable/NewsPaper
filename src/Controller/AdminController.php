588131b579d6f3235954e334c9b82c70c02bddc9
Philisavailable <bertola.philippe@gmail.com>
2023-04-13 15:27:07 +0200

    Show User + modify Role USER

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Entity\Category;
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,10 +29,12 @@ class AdminController extends AbstractController
 
         $categories = $entityManager->getRepository(Category::class)->findBy(['deletedAt' => null]);
         $articles = $entityManager->getRepository(Article::class)->findBy(['deletedAt' => null]);
+        $users = $entityManager->getRepository(User::class)->findBy(['deletedAt' => null]);
 
         return $this->render('admin/show_dashboard.html.twig', [
             'categories' => $categories,
-            'articles' => $articles
+            'articles' => $articles,
+            'users' => $users
         ]);
     } // end showDashboard()
 
@@ -40,10 +44,29 @@ class AdminController extends AbstractController
         $categories = $entityManager->getRepository(Category::class)->findAllArchived();
         $articles = $entityManager->getRepository(Article::class)->findAllArchived();
 
+
         return $this->render('admin/show_archive.html.twig', [
             'categories' => $categories,
             'articles' => $articles
         ]);
-    }
+    } // end showArchives()
+
+    #[Route('/modifier-role-user/{id}', name: 'modify_user_role', methods: ['GET'])]
+    public function modifyUserRole(User $user, UserRepository $repository): Response
+    {
+        # - 1 : recuperer son role 2 : modifier le role 3 : save en BDD
+
+        if(in_array('ROLE_USER', $user->getRoles())) {
+            $user->setRoles(['ROLE_ADMIN']);
+        }
+        else {
+            $user->setRoles(['ROLE_USER']);
+        }
+
+        $repository->save($user, true);
+
+        $this->addFlash('success', "Le rôle a bien été modifié.");
+        return $this->redirectToRoute('show_dashboard');
+    } // end modifyRole()
 
 } // end class
